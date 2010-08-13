51fec751ba767af6b9e96bd026a669b1fc72061e
Dmitry Kichenko <dmitrykichenko@gmail.com>
2010-08-12 22:12:00 -0400

    Fixed indentation.

--- dropmvc.js
+++ dropmvc.js
@@ -31,9 +31,9 @@ if(jQuery === undefined) throw 'DropMVC requires jQuery';
             this._elmt = $(this._selector, context);
             delete this._selector;
         },
-		bind: function(eventType, handler){
-			this._view.bind(this, eventType, handler);
-		},
+        bind: function(eventType, handler){
+            this._view.bind(this, eventType, handler);
+        },
         elmt: function(){ return this._elmt; }
 
     }
@@ -50,23 +50,23 @@ if(jQuery === undefined) throw 'DropMVC requires jQuery';
     View.prototype = {
         addCtrl: function(name,control) {
             if(this[name] !== undefined) throw(name+" is already defined.");
-            this[name] = (control instanceof Control) ? control : new Control(control);
-			this._controls.push({c: this[name]});
-			this[name]._view = this;
+                this[name] = (control instanceof Control) ? control : new Control(control);
+            this._controls.push({c: this[name]});
+            this[name]._view = this;
             this[name]._initSelector(this._container);
         },
-		bind: function(control, eventType, handler) {
+        bind: function(control, eventType, handler) {
             var c = 0;
             while(this._controls[c].c != control) c++; // aa-haha
-            this._controls[c].h = handler;
-			this._container.bind(eventType, $.proxy( this._eventHandler, this) );
-		},
-		_eventHandler: function(e){
+                this._controls[c].h = handler;
+            this._container.bind(eventType, $.proxy( this._eventHandler, this) );
+        },
+        _eventHandler: function(e){
             var c;
             for(var i=0; i < this._controls.length; i++) 
                 if (this._controls[i].c._elmt.get(0) == e.target)
-                   this._controls[i].h.call(this._controller, e); 
-		}
+                    this._controls[i].h.call(this._controller, e); 
+        }
     }
 
     // controllers react to events and do stuff to views
@@ -78,11 +78,11 @@ if(jQuery === undefined) throw 'DropMVC requires jQuery';
     }
 
     Controller.prototype = {
-		// add a view to the controller
-		view: function(name, view) {
-			this[name] = this._views[name] = view;
-			view._controller = this;
-		}
+        // add a view to the controller
+        view: function(name, view) {
+            this[name] = this._views[name] = view;
+            view._controller = this;
+        }
     }
 	
 	// finally, announce self to the world
