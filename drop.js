1bf468dc3cce8fe5ea73d272dbaf50507aa3739b
Dmitry Kichenko <dmitrykichenko@gmail.com>
2010-08-20 22:47:26 -0400

    Clearer documentation and some renaming.

--- drop.js
+++ drop.js
@@ -3,9 +3,9 @@ if(jQuery === undefined) throw 'dropjs requires jQuery';
 (function($){
     // primary object everything else derives from
     function DropObject() { }
-    // merges given object into a prototype of a new
-    // function and returns the result that can be used
-    // with the 'new' keyword
+    // Simplified inheritance. Merge given object into a prototype of a 
+    // new function and return the result that can be used with the 'new' 
+    // keyword
     DropObject.breed = function(o) {
         var newobj = function(){ 
             ( this.init || this._init ).apply( this, arguments );
@@ -16,9 +16,11 @@ if(jQuery === undefined) throw 'dropjs requires jQuery';
         return newobj;
     }
 
-    // controls are buttons, checkboxes, anything that could react to actions
+    // Controls are buttons, checkboxes, anything that could emit events.
+    // They are a basic unit of presentation, and are grouped together under
+    // a View.
     var Control = DropObject.breed({
-        init: function(elmt) {
+        _init: function(elmt) {
             this._elmt = false;
             this._view = undefined;
             this._selector = undefined;
@@ -32,34 +34,45 @@ if(jQuery === undefined) throw 'dropjs requires jQuery';
                 delete this.selector;
             }
         },
-        // initialize the jQuery selector within scope of context to speed things up.
+        // Initialize the jQuery selector within scope of context. Run upon
+        // assigning this controler to a view to speed things up.
         _initSelector: function(context){ 
             this._elmt = $(this._selector, context);
             delete this._selector;
         },
+        // Bind this control to a given function. The function is always 
+        // run in the context of the Controller this controler belongs to.
         bind: function(eventType, handler){
             this._view.bind(this, eventType, handler);
-        },
-        elmt: function(){ return this._elmt; }
-
+        }
     });
 
+    // Views are a generic 'container' that contains Control objects, 
+    // listens for bubbling events, and calls corresponding controller 
+    // methods if the events are emitted from one of its controls.
     var View = DropObject.breed({
-        init: function(container, controls) {
-            this._container = $(container);
+        // View's contructor takes in a jQuery selector and an optional object
+        // filled with controls to assign to this view
+        _init: function(selector) {
+            var controls = arguments[1];
+            this._container = $(selector);
             this._controller = undefined;
             this._controls = []; // keep array of all controls as well
 
             // wrap control in jQuery object if necessary
-            for(var c in controls) this.addControl(c, controls[c]);
+            for(var c in controls) this.control(c, controls[c]);
         },
-        addControl: function(name,control) {
+        // Add a Control object to this view. Takes in a name of the
+        // control and an object filled out with methods for this control.
+        control: function(name,control) {
             if(this[name] !== undefined) throw(name+" is already defined.");
                 this[name] = (control instanceof Control) ? control : new Control(control);
             this._controls.push({c: this[name]});
             this[name]._view = this;
             this[name]._initSelector(this._container);
         },
+        // Bind a handler (most often a controller method) to a given control
+        // and event type
         bind: function(control, eventType, handler) {
             var c = 0;
             while(this._controls[c].c != control) c++; // aa-haha
@@ -69,6 +82,8 @@ if(jQuery === undefined) throw 'dropjs requires jQuery';
             if(typeof events == 'undefined' || typeof events[eventType] == 'undefined')
                 this._container.bind(eventType, $.proxy( this._eventHandler, this ) );
         },
+        // Handle DOM events from controls and redirect them to the appropriate
+        // controller methods
         _eventHandler: function(e){
             var c;
             for(var i=0; i < this._controls.length; i++) 
@@ -84,9 +99,10 @@ if(jQuery === undefined) throw 'dropjs requires jQuery';
     });
 
 
-    // controllers react to events and do stuff to views
+    // Controllers connect views, controls and models together. E.g. a control 
+    // is typically assigned to a controller method.
     var Controller = DropObject.breed({ 
-        init: function(o){
+        _init: function(o){
             this._views = {}; 
             $.extend(this, o); 
             var init = (o.init || o._init);
